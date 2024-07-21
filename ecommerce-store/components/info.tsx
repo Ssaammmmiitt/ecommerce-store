a87afd47ac2660bace20454c4aa28847fb6232e9
Sammit Poudyal <poudyal.sammit@gmail.com>
2024-07-22 02:03:56 +0545

    feat: Add Add To Cart button to Info component
    - Added an "Add To Cart" button to the Info component for users to easily add products to their shopping cart
    
    Resolves: #123

--- ecommerce-store/components/info.tsx
+++ ecommerce-store/components/info.tsx
@@ -1,5 +1,10 @@
+"use client";
+
+import { ShoppingCartIcon } from "lucide-react";
+
 import { Product } from "@/types";
-import Currency from "./ui/currency";
+import Currency from "@/components/ui/currency";
+import Button from "@/components/ui/button";
 
 
 interface InfoProps {
@@ -33,6 +38,12 @@ const Info:React.FC<InfoProps> = ({
                 <div className="h-6 w-6 rounded-full border border-gray-600"
                 style={{backgroundColor:data?.color?.value}} />
             </div>
+            </div>
+            <div className="mt-10 flex items-center gap-x-3">
+                <Button className="flex items-center gap-x-2">
+                    Add To Cart
+                    <ShoppingCartIcon/>
+                </Button>
             </div>
         </div>
       );
